c7a124a841344c7e353b635bb53f29ccf57d8430
Deepak <118921455+arcatra@users.noreply.github.com>
2026-02-27 22:55:29 +0530

    Emergency Commit

--- tictoc/src/tictoc/utils/Board.java
+++ tictoc/src/tictoc/utils/Board.java
@@ -8,74 +8,74 @@ import tictoc.GameEntry;
 
 public class Board {
 
-    private ArrayList<BoardBox[]> board = new ArrayList<>();
-    private int rows;
-    private int columns;
-    GameEntry gameObj;
+  private ArrayList<BoardBox[]> board = new ArrayList<>();
+  private int rows;
+  private int columns;
+  GameEntry gameObj;
 
-    public Board(int rowSize, int columnSize, GameEntry gameObj) {
-        this.rows = rowSize;
-        this.columns = columnSize;
-        this.gameObj = gameObj;
+  public Board(int rowSize, int columnSize, GameEntry gameObj) {
+    this.rows = rowSize;
+    this.columns = columnSize;
+    this.gameObj = gameObj;
 
+  }
+
+  public void buildBoard() {
+
+    System.out.println("Trying to build the board");
+    int boxId = 1;
+    for (int row = 0; row < this.rows; row++) {
+      BoardBox[] boxes = new BoardBox[this.columns];
+
+      for (int column = 0; column < this.columns; column++) {
+        boxes[column] = new BoardBox(Integer.toString(boxId), GameEntry.RESET);
+        gameObj.boxMap.put(boxId, new int[] { row, column });
+
+        boxId++;
+      }
+
+      this.board.add(boxes);
     }
 
-    public void buildBoard() {
+    System.out.printf(
+        "Success, constructed board with %d rows and %d columns\n\n", this.rows, this.columns);
+  }
 
-        System.out.println("Trying to build the board");
-        int boxId = 1;
-        for (int row = 0; row < this.rows; row++) {
-            BoardBox[] boxes = new BoardBox[this.columns];
+  public void displayBoard() {
+    String boxString = "|  0  |";
+    int repeatCount = boxString.length() - 2;
 
-            for (int column = 0; column < this.columns; column++) {
-                boxes[column] = new BoardBox(Integer.toString(boxId), GameEntry.RESET);
-                gameObj.boxMap.put(boxId, new int[] { row, column });
+    for (BoardBox[] rowValues : this.board) {
+      System.out.println(("+" + "-".repeat(repeatCount)).repeat(this.columns) + "+");
+      for (BoardBox box : rowValues) {
+        String boxSymbol = box.getSymbol();
+        String symbolColor = box.getSymbolColor();
 
-                boxId++;
-            }
+        boxString = (boxSymbol.length() < 2 ? "|  " : "| ") + symbolColor + boxSymbol + GameEntry.RESET
+            + "  ";
 
-            this.board.add(boxes);
-        }
+        System.out.print(boxString);
+      }
+      System.out.println("|");
+    }
+    System.out.println(("+" + "-".repeat(repeatCount)).repeat(this.columns) + "+");
 
-        System.out.printf(
-                "Success, constructed board with %d rows and %d columns\n\n", this.rows, this.columns);
+  }
+
+  public BoardBox getBoardBox(int currRow, int currColumn) {
+    BoardBox box = this.board.get(currRow)[currColumn];
+
+    return box;
+  }
+
+  public void clearBoard() {
+
+    if (this.board.size() > 1) {
+      this.board.clear();
+
+      System.out.println("Board has been cleared!\n");
     }
 
-    public void displayBoard() {
-        String boxString = "|  0  |";
-        int repeatCount = boxString.length() - 2;
-
-        for (BoardBox[] rowValues : this.board) {
-            System.out.println(("+" + "-".repeat(repeatCount)).repeat(this.columns) + "+");
-            for (BoardBox box : rowValues) {
-                String boxSymbol = box.getSymbol();
-                String symbolColor = box.getSymbolColor();
-
-                boxString = (boxSymbol.length() < 2 ? "|  " : "| ") + symbolColor + boxSymbol + GameEntry.RESET
-                        + "  ";
-
-                System.out.print(boxString);
-            }
-            System.out.println("|");
-        }
-        System.out.println(("+" + "-".repeat(repeatCount)).repeat(this.columns) + "+");
-
-    }
-
-    public BoardBox getBoardBox(int currRow, int currColumn) {
-        BoardBox box = this.board.get(currRow)[currColumn];
-
-        return box;
-    }
-
-    public void clearBoard() {
-
-        if (this.board.size() > 1) {
-            this.board.clear();
-
-            System.out.println("Board has been cleared!\n");
-        }
-
-    }
+  }
 
 }
--- tictoc/src/tictoc/utils/BoardBox.java
+++ tictoc/src/tictoc/utils/BoardBox.java
@@ -1,40 +1,40 @@
 package tictoc.utils;
 
 public class BoardBox {
-    private String symbol;
-    private String symColor;
-    private int id;
+  private String symbol;
+  private String symColor;
+  private int id;
 
-    public BoardBox(String symbol, String symColor) {
-        this.symbol = symbol;
-        this.symColor = symColor;
-        this.id = Integer.parseInt(symbol);
+  public BoardBox(String symbol, String symColor) {
+    this.symbol = symbol;
+    this.symColor = symColor;
+    this.id = Integer.parseInt(symbol);
 
-    }
+  }
 
-    public boolean isBoxEmpty() {
-        return (!this.symbol.equals("X") && !this.symbol.equals("O"));
+  public boolean isBoxEmpty() {
+    return (!this.symbol.equals("X") && !this.symbol.equals("O"));
 
-    }
+  }
 
-    public void setBoxSymbol(String newSymbol, String newSymColor) {
-        this.symbol = newSymbol;
-        this.symColor = newSymColor;
+  public void setBoxSymbol(String newSymbol, String newSymColor) {
+    this.symbol = newSymbol;
+    this.symColor = newSymColor;
 
-    }
+  }
 
-    public String getSymbol() {
-        return this.symbol;
+  public String getSymbol() {
+    return this.symbol;
 
-    }
+  }
 
-    public String getSymbolColor() {
-        return this.symColor;
+  public String getSymbolColor() {
+    return this.symColor;
 
-    }
+  }
 
-    public int getBoxId() {
-        return this.id;
-    }
+  public int getBoxId() {
+    return this.id;
+  }
 
 }
